940cd4f0853726b9825bf053245fc6a79407edc1
Galeeva <galeeva.alm@gmail.com>
2023-04-22 17:23:45 +0300

    second

--- app/src/main/java/com/example/tinkoffcup/custom/HeaderView.kt
+++ app/src/main/java/com/example/tinkoffcup/custom/HeaderView.kt
@@ -3,7 +3,7 @@ package com.example.tinkoffcup.custom
 import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
-import android.widget.LinearLayout
+import android.view.View
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 
@@ -25,68 +25,15 @@ class HeaderView @JvmOverloads constructor(
         )
         header.text =
             styledAttr.getText(com.example.tinkoffcup.R.styleable.HeaderView_headerText)
-        subHeader.text =
+        val subHeaderText =
             styledAttr.getText(com.example.tinkoffcup.R.styleable.HeaderView_subHeaderText)
-        header.setTextColor(
-            styledAttr.getColor(com.example.tinkoffcup.R.styleable.HeaderView_headerColor, 0)
-        )
+
+        if (!subHeaderText.isNullOrEmpty())
+            subHeader.text = subHeaderText
+        else subHeader.visibility = View.GONE
         subHeader.setTextColor(
-            styledAttr.getColor(com.example.tinkoffcup.R.styleable.HeaderView_subHeaderColor, 0))
+            styledAttr.getColor(com.example.tinkoffcup.R.styleable.HeaderView_subHeaderColor, 0)
+        )
     }
 
-    /*  private lateinit var _headerTextView: TextView
-      private lateinit var _subHeaderTextView: TextView
-      private lateinit var _iconBackground: View
-      private lateinit var _icon: View
-
-      init {
-          this.orientation = VERTICAL
-          if (!::_headerTextView.isInitialized) {
-              initHeaderTextView()
-          }
-          if (!::_subHeaderTextView.isInitialized) {
-              initSubHeaderTextView()
-          }
-      }
-
-      private fun initHeaderTextView() {
-          _headerTextView = TextView(context, null, 0, R.style.headertext)
-          _headerTextView
-          _headerTextView.apply {
-              updateMargin(top = 16.pxToDp(resources), left = 20.pxToDp(resources))
-          }
-
-          addView(_headerTextView, 0)
-      }
-
-      fun setHeaderText(text: String) {
-          _headerTextView.text = text
-          if (text != "") {
-              _headerTextView.visibility = VISIBLE
-              _headerTextView.text = text
-          } else {
-              _headerTextView.visibility = GONE
-          }
-
-      }
-
-      fun setSubHeaderText(text: String) {
-          _subHeaderTextView = TextView(context, null, 0, R.style.subheadertext)
-          if (text != "") {
-              _subHeaderTextView.visibility = VISIBLE
-              _subHeaderTextView.text = text
-          } else {
-              _subHeaderTextView.visibility = GONE
-          }
-      }
-
-      private fun initSubHeaderTextView() {
-          _subHeaderTextView = TextView(context)
-          _subHeaderTextView
-          _subHeaderTextView.apply {
-              updateMargin(top = 16.pxToDp(resources), left = 20.pxToDp(resources))
-          }
-          addView(_subHeaderTextView, 0)
-      }
-  */
 }
